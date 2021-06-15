b4088c55ae44a51193539c8cd45f3fe88bf57a70
Théo BIET <dev.theobiet@gmail.com>
2021-06-15 17:25:34 +0200

    WIP api GET

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const message = "HELLO"
-  axios.get('/api')
+  const message = "WRONG"
+  axios.get('http://theobiet.fr/api')
     .then(function (response) {
       console.log(response);
+      return <div className='m-2'>{response.data.message}</div>
     })
     .catch(function (error) {
       console.log(error);
+      return <div className='m-2'>{message}</div>
     })
 
-  return <div className='m-2'>{message}</div>
 }
 
 export default App;

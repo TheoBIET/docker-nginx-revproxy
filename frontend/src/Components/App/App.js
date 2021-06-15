0b9d847bde7a5feed77212e96e817f27a296775b
Théo BIET <dev.theobiet@gmail.com>
2021-06-15 17:31:28 +0200

    Show response in DOM

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { render } from "sass";
 
 const App = () => {
-  const message = "WRONG"
-  axios.get('http://theobiet.fr/api')
-    .then(function (response) {
-      console.log(response);
-      return <div className='m-2'>{response.data.message}</div>
-    })
-    .catch(function (error) {
-      console.log(error);
-      return <div className='m-2'>{message}</div>
-    })
-
-}
+    let message = "";
+    axios
+        .get("http://theobiet.fr/api")
+        .then(function (response) {
+            console.log(response);
+            return render(<div className="m-2">{response.data.message}</div>);
+        })
+        .catch(function (error) {
+            console.log(error);
+            return render(<div className="m-2">{message}</div>);
+        });
+};
 
 export default App;
